0d6531bffffa0be0b0f3668240b4c504b27f24c2
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-10-24 03:03:06 +0800

    Update MalworkMod.java

--- src/malwork/MalworkMod.java
+++ src/malwork/MalworkMod.java
@@ -21,7 +21,7 @@ public class MalworkMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("malwork-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("malwork-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
