58eccf451ed3df9ff53c76306c79b5d57360ad90
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-10-24 20:12:55 +0800

    Bugfix 6

--- src/malwork/content/MWUnitTypes.java
+++ src/malwork/content/MWUnitTypes.java
@@ -14,64 +14,12 @@ import mindustry.type.*;
 import mindustry.world.meta.*;
 
 public class MWUnitTypes {
-	/*
-	private static final Entry<Class<? extends Entityc>, Prov<? extends Entityc>>[] types = new Entry[]{
-		
-	};
 	
-	private static final ObjectIntMap<Class<? extends Entityc>> idMap = new ObjectIntMap<>();
-	/*
-    /**
-     * Internal function to flatmap {@code Class -> Prov} into an {@link Entry}.
-     * @author GlennFolker
-     */
-	 /*
-    private static <T extends Entityc> Entry<Class<T>, Prov<T>> prov(Class<T> type, Prov<T> prov){
-        Entry<Class<T>, Prov<T>> entry = new Entry<>();
-        entry.key = type;
-        entry.value = prov;
-        return entry;
-    }
-	/*
-    /**
-     * Setups all entity IDs and maps them into {@link EntityMapping}.
-     * @author GlennFolker
-     */
-	/*
-    private static void setupID(){
-        for(
-            int i = 0,
-            j = 0,
-            len = EntityMapping.idMap.length;
-
-            i < len;
-
-            i++
-        ){
-            if(EntityMapping.idMap[i] == null){
-                idMap.put(types[j].key, i);
-                EntityMapping.idMap[i] = types[j].value;
-
-                if(++j >= types.length) break;
-            }
-        }
-    }
-	*/
-    /**
-     * Retrieves the class ID for a certain entity type.
-     * @author GlennFolker
-     */
-	 /*
-    public static <T extends Entityc> int classID(Class<T> type){
-        return idMap.get(type, -1);
-    }
-	*/
 	public static UnitType 
 	
 	legged1;
 	
 	public static void load(){
-		//setupID(); -- Disabled because there is no custom types.
 		
 		//Region Legged
 		legged1 = new UnitType("legged1"){{
