7f7baf2a80a0401960c2a7cdf8bdf6feea491d60
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-10-26 02:12:48 +0800

    Update MWUnitTypes.java

--- src/malwork/content/MWUnitTypes.java
+++ src/malwork/content/MWUnitTypes.java
@@ -31,32 +31,32 @@ public class MWUnitTypes {
 	public static void load(){
 		
 		//Region Legged
-		legged1 = new UnitType("legged1"){
+		legged1 = new ErekirUnitType("legged1"){
 			{
 				drag = 0.1f;
-				speed = 1.1f;
+				speed = 0.6f;
 				hitSize = 20f;
 				rotateSpeed = 1.6f;
 				lockLegBase = true;
 				legContinuousMove = true;
 				legStraightness = 0.4f;
 				baseLegStraightness = 0.5f;
-				outlineColor = Pal.darkOutline;
 				
 				groundLayer = Layer.legUnit;
 				
 				legCount = 6;
-				legLength = 30f;
-				legForwardScl = 2f;
-				legLengthScl = 1f;
+				legLength = 20f;
 				legMoveSpace = 1.05f;
-				rippleScale = 1.2f;
-				stepShake = 0.5f;
 				legGroupSize = 3;
-				legExtension = -3f;
-				legBaseOffset = 7f;
-				legStraightLength = 0.4f;
-				legMaxLength = 1.1f;
+				legExtension = -5f;
+				legBaseOffset = 15f;
+				
+				parts.add(new PartRegion("-side"){{
+					layerOffset = 0.01f;
+					x = 46f;
+					y = 16f;
+					mirror = true;
+				}});
 			}
 			
 			@Override
