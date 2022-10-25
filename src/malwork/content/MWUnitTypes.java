5f5d9c1f01381a89a52d11f85f37ec79df2e11c3
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-10-26 02:18:30 +0800

    bugfix 3

--- src/malwork/content/MWUnitTypes.java
+++ src/malwork/content/MWUnitTypes.java
@@ -7,7 +7,7 @@ import arc.graphics.*;
 import arc.graphics.g2d.*;
 import mindustry.gen.*;
 import mindustry.type.*;
-import mindustry.type.units.*;
+import mindustry.type.unit.*;
 import mindustry.content.*;
 import mindustry.graphics.*;
 import mindustry.ai.types.*;
